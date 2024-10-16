2fc050e8066dd06eff502c8cd34beab379738070
nickturek <njt53@pitt.edu>
2024-10-16 14:12:55 -0400

    Nick's commit

--- simple.py
+++ simple.py
@@ -1,3 +1,8 @@
 name = input('Enter a name: ')
 num = int(input('Enter an integer: '))
 print(f"{name} " * num)
+
+num1 = int(input('Enter an integer: '))
+num2 = int(input('Enter another integer: '))
+product =  num1 * num2
+print(f'The product of {num1} and {num2} is {product}')
